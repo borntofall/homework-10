8a52ec60e58459194be78d97071bccf223e5eac4
borntofall <vsexobnyal@yandex.ru>
2024-06-07 03:00:39 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Task 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф.И.О. сотрудника -  " + fullName);
+
+        System.out.println("Task 2");
+
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("Task 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "e");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullNameTrue);
+
     }
 }
